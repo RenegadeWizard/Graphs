b1f57a481274c4fe3a81c22325c1cf0ab0b2e564
Krzysztof Sychla <ksychla5@gmail.com>
2018-04-18 16:16:41 +0200

    Initial Commit

---
+++ Graphs/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Graphs
+//
+//  Created by Krzysztof Sychla on 18.04.2018.
+//  Copyright © 2018 Krzysztof Sychla. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
